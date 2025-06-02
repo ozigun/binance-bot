6a9e9211f7098b6f6b5a5c1728f153cfb44cd3ee
ozigun <ozigun1988@gmail.com>
2025-06-02 18:51:19 +0300

    fix: css tools

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import "../styles/globals.css";
+import "../styles/login.css";
 
 export default function Login() {
   const [apiKey, setApiKey] = useState("");
